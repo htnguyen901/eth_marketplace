750979e7d297fcc0d5da9baf735372d7522380dd
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2021-06-01 23:39:24 +0700

    add sellProduct & transfer fund

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -12,7 +12,7 @@ contract Marketplace {
 		uint id;
 		string name;
 		uint price;
-		address owner;
+		address payable owner;
 		bool purchased;
 	}
 
@@ -20,7 +20,15 @@ contract Marketplace {
 		uint id,
 		string name,
 		uint price,
-		address owner,
+		address payable owner,
+		bool purchased
+	);
+
+	event ProductPurchased(
+		uint id,
+		string name,
+		uint price,
+		address payable owner,
 		bool purchased
 	);
 
@@ -42,4 +50,41 @@ contract Marketplace {
 		// Trigger an event
 		emit ProductCreated(productCount, _name, _price, msg.sender, false);
 	}
+
+	function purchaseProduct(uint _id) public payable {
+		// Fetch the product
+		Product memory _product = products[_id]; // get the product from mapping then copy it to a local (memory) variable _product
+
+		// Fetch the owner
+		address payable _seller = _product.owner;
+
+		// Make sure the product is valid
+		require(_product.id > 0 && _product.id <= productCount);
+
+		// Require that there is enough Ether in the transaction
+		require(msg.value >= _product.price);
+
+		// Require that the product has not been purchased already
+		require(!_product.purchased);
+
+		// Require that the buyer is not the seller
+		require(_seller != msg.sender);
+
+		// Transfer ownership to the buyer
+		_product.owner = msg.sender;
+
+		// Mark as purchased
+		_product.purchased = true;
+
+		// Update the product
+		products[_id] = _product; // put back to product mapping
+
+		// Pay the seller by sending them Ether
+		address(_seller).transfer(msg.value); // take the Ether that get from seller and send to buyer
+
+		// Trigger an event
+		emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
+	}
+
+
 }
