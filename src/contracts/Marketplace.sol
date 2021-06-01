81d80145ee9ef594fe1fa93933b13b11a557856a
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2021-06-01 18:49:49 +0700

    add createProduct

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -4,8 +4,42 @@ pragma solidity ^0.5.0;
 contract Marketplace {
 	string public name;
 
-	constructor() public {
-		name = "Toto is geilord";
+	uint public productCount = 0; //to count products so that mapping doesnt return blank Product
+
+	mapping(uint => Product) public products;
+
+	struct Product {
+		uint id;
+		string name;
+		uint price;
+		address owner;
+		bool purchased;
 	}
-	
+
+	event ProductCreated(
+		uint id,
+		string name,
+		uint price,
+		address owner,
+		bool purchased
+	);
+
+	constructor() public {
+		name = "ETH Marketplace";
+	}
+
+	function createProduct(string memory _name, uint _price) public {
+		// Require a name
+		require(bytes(_name).length > 0);
+
+		// Require a valid price
+		require(_price > 0);
+
+		// Increment product count
+		productCount ++;
+		// Create product
+		products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+		// Trigger an event
+		emit ProductCreated(productCount, _name, _price, msg.sender, false);
+	}
 }
