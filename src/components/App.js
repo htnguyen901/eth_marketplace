75b541604fa1887cafb6b8558108f4e48c86574b
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2021-06-03 14:47:00 +0700

    load smart contract

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,61 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo3.png';
 import './App.css';
+import Marketplace from '../abis/Marketplace.json';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+      // Modern dapp browsers...
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+          
+        // Request account access if needed
+        await window.ethereum.enable();
+          
+      }
+
+      // Legacy dapp browsers...
+      else if (window.web3) {
+          window.web3 = new Web3(window.web3.currentProvider);
+      }
+      // Non-dapp browsers...
+      else {
+          window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+      };
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+    const networkData = Marketplace.networks[networkId]
+    if (networkData) {
+      const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
+      console.log(marketplace)
+    } else {
+      window.alert("Marketplace contract not deployed to detected network.")
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      productCount: 0,
+      products: [],
+      loading: true
+    }
+  }
+
   render() {
     return (
       <div>
@@ -15,13 +68,18 @@ class App extends Component {
           >
             TAME Marketplace
           </a>
+        <ul className = "navbar-nav px-3">
+          <li className = "nav-item test-nowrap d-none d-sm-none d-sm-block">
+            <small className = "text-white"> <span id="account"> {this.state.account} </span> </small>
+          </li>
+        </ul>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <a
-                  href="/"
+                  href=""
                   target="_blank"
                   rel="noopener noreferrer"
                 >
