e5538b7ffb7ce29177046a44150e187235e00976
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2021-06-03 14:58:06 +0700

    refactoring Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Web3 from 'web3';
 import logo from '../logo3.png';
 import './App.css';
 import Marketplace from '../abis/Marketplace.json';
+import Navbar from './Navbar'
 
 class App extends Component {
 
@@ -59,21 +60,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            TAME Marketplace
-          </a>
-        <ul className = "navbar-nav px-3">
-          <li className = "nav-item test-nowrap d-none d-sm-none d-sm-block">
-            <small className = "text-white"> <span id="account"> {this.state.account} </span> </small>
-          </li>
-        </ul>
-        </nav>
+      <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
